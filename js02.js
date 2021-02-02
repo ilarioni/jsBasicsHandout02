c904609de4c2fac30a28047be07edd0d0b2a94af
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-02 16:10:15 +0100

    JS method
    slice()

--- js02.js
+++ js02.js
@@ -24,3 +24,21 @@ console.log(pos2);
 var pos3 = str.search("locate");
 console.log(pos3);
 
+// Extracting String Parts
+// slice(start, end)
+// substring(start, end);
+// substr(start, length);
+
+// >>>> slice
+var txt1 = "Apple, Banana, Kiwi";
+var res1 = txt1.slice(7, 13);
+console.log(res1);
+
+var res2 = txt1.slice(-12, -6);
+console.log(res2); 
+
+var res3 = txt1.slice(7);
+console.log(res3);
+
+var res4 = txt1.slice(-12);
+console.log(res4);
